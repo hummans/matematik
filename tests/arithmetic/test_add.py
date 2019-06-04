ae35bcbb3ed534fc8e9ff29af1877091822ffc73
Demir Antay <demir99antay@gmail.com>
2019-06-04 15:31:27 +0300

    started adding tests of add, multiply, subtract modules

---
+++ tests/arithmetic/test_add.py
@@ -0,0 +1,26 @@
+import unittest
+from .matematik.arithmetic.add import add, add_nums, arr_sum, tup_sum
+
+
+class Test_add(unittest.TestCase):
+
+    def test_if_returning_type_is_int(self):
+        pass
+
+    def test_if_function_returns_a_true_value(self):
+        self.assertEqual(add(5, 10), 15)
+
+    def test_if_it_works_with_negative_nums(self):
+        pass
+
+
+class Test_add_nums(unittest.TestCase):
+
+    def test_if_reurning_type_is_int(self):
+        pass
+
+    def test_if_function_returns_a_true_value(self):
+        pass
+
+    def test_if_it_works_with_negative_nums(self):
+        pass
