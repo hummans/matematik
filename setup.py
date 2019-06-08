8a5ac5b6f25b7c5962d2b65b2d9d18e84a54fc9a
Demir Antay <demir99antay@gmail.com>
2019-06-08 15:46:24 +0300

    updated to version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="matematik",
-    version="0.1.0",
+    version="1.0.0",
     author="Demir Antay",
     author_email="demir99antay@gmail.com",
     description="An package for solving math equations",
